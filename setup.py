ce690f6dce03307f9305d89edec444656d5fb5ed
urigott <45831390+urigott@users.noreply.github.com>
2025-03-07 00:55:45 +0200

    v.0.1.2 (#4)
    * add publish in TestPyPi
    
    * update workflow.yml: install uv
    
    * update versions
    
    * versions adjustment
    
    * update requirements.txt to minimal
    
    * update version controlling, realease version 0.1.1
    
    * update workflow.yml: publish-to-pypi
    
    * update version to 0.1.2

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
+import tomli
+
+with open('pyproject.toml', 'rb') as f:
+    version = tomli.load(f)['project']['version']
 
 setup(
     name='whatifact',
-    version='0.1.0',
+    version=version,
     packages=find_packages(),
     test_suite='tests',
+    url='http://github.com/urigott/whatifact',
+    author_email='urigott@gmail.com',
+    license='MIT',
+    install_requires=[
+        "numpy>=1.26.0",
+        "pandas>=1.1.0",
+        "shiny>=1.0.0",
+    ]
+
 )
