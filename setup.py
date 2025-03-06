302b1f3f30699523242c7f5ccc1ae7bd040af5b7
urigott <urigott@gmail.com>
2025-03-06 23:39:23 +0200

    add publish in TestPyPi

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     test_suite='tests',
+    url='http://github.com/urigott/whatifact',
+    author_email='urigott@gmail.com',
+    license='MIT',
+
 )
