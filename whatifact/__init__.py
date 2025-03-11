70fad233bb586b34c560499f2e1b7089bc3a7733
urigott <urigott@gmail.com>
2025-03-12 00:27:11 +0200

    update version

--- whatifact/__init__.py
+++ whatifact/__init__.py
@@ -1,9 +1,5 @@
 from .whatifact import whatifact
-import tomli
-
-with open('pyproject.toml', 'rb') as f:
-    version = tomli.load(f)['project']['version']
 
 __all__ = ["whatifact"]
 __author__ = "Uri Gottlieb <urigott@gmail.com>"
-__version__ = version
+__version__ = '0.1.7'
